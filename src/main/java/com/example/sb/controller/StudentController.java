d321497f9fe470c55b34fcd2119a9fa8c430ae44
boga_david <spoter49@gmail.com>
2023-02-10 11:37:19 +0530

    Added imported library in test page

--- src/main/java/com/example/sb/controller/StudentController.java
+++ src/main/java/com/example/sb/controller/StudentController.java
@@ -12,12 +12,14 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import com.example.sb.service.StudentService;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @Controller
 public class StudentController {
+    // Second Page error and success messages
     String error = "";
     String success = "";
 
@@ -30,6 +32,7 @@ public class StudentController {
     @Autowired
     private ClassDataService classDataService;
 
+    //Initial Page or Landing Page
     @RequestMapping("/")
     public String homePage(Model model) {
         error = "";
@@ -37,13 +40,13 @@ public class StudentController {
         List<ClassData> classDataList = classDataService.getAllClassData();
         List<ClassDetails> classDetailsList = classDetailService.getAllClassDetails();
         List<ClassDetails> temp = new ArrayList<>();
+
+        //Calculate and update the available seat in a class
         for (ClassDetails details : classDetailsList) {
             int t = 0;
-            for (ClassData classData : classDataList) {
-                if (Objects.equals(details.getClassName(), classData.getClassName())) {
+            for (ClassData classData : classDataList)
+                if (Objects.equals(details.getClassName(), classData.getClassName()))
                     t++;
-                }
-            }
             ClassDetails classDetails = new ClassDetails();
             classDetails.setAvailability(100 - t);
             classDetails.setClassName(details.getClassName());
@@ -54,32 +57,28 @@ public class StudentController {
         return "index";
     }
 
+    //Seat Management Page
     @RequestMapping("/second/{className}")
     public String secondPage(@PathVariable(value = "className") String className, Model model) {
         List<StudentDetails> studentDetailsList = studentService.getAllStudentDetails();
         List<ClassData> classDataList = classDataService.getAllClassData();
         List<ClassData> filterClassData = new ArrayList<>();
-        for (ClassData data : classDataList) {
-            if (Objects.equals(data.getClassName(), className)) {
+        for (ClassData data : classDataList)
+            if (Objects.equals(data.getClassName(), className))
                 filterClassData.add(data);
-            }
-        }
 
         for (int j = 0; j < studentDetailsList.size(); j++)
             for (ClassData classData : classDataList)
                 if (Objects.equals(classData.getRollNo(), studentDetailsList.get(j).getRollNo()))
                     studentDetailsList.remove(j);
 
+        //Get seat data from database, show in the page
         List<List<Integer>> a = emptyArray();
         for (ClassData filterClassDatum : filterClassData) {
             int t = (filterClassDatum.getSeatNo() - 1) / 10;
-//            System.out.println((filterClassData.get(i).getSeatNo() - 1)/10);
             List<Integer> t1 = a.get(t);
-//            System.out.println(t1);
             t1.set((filterClassDatum.getSeatNo() - 1) % 10, filterClassDatum.getRollNo().intValue());
             a.set(t, t1);
-//            System.out.println(t1);
-//            System.out.println(t);
         }
         model.addAttribute("error", error);
         model.addAttribute("success", success);
@@ -89,25 +88,32 @@ public class StudentController {
         return "second";
     }
 
+    //Show Report Page
     @RequestMapping("/third/{className}")
     public String thirdPage(@PathVariable(value = "className") String className, Model model) {
         List<ClassData> classDataList = classDataService.getAllClassData();
-        List<StudentDetails> temp = new ArrayList<>();
-        for (ClassData classData : classDataList) {
+        List<ClassData> temp = new ArrayList<>();
+
+        //Show the allocated seat in the page
+        for (ClassData classData : classDataList)
             if (Objects.equals(classData.getClassName(), className))
-                temp.add(studentService.getStudent(classData.getRollNo()));
-        }
+                temp.add(classData);
+
         model.addAttribute("listStudentDetails", temp);
         return "third";
     }
 
+    //Unallocated the seat for the student
     @RequestMapping("/deleteSeat/{className}")
     public String deleteSeat(@RequestParam MultiValueMap<String, String> parameters, @PathVariable(value = "className") String className) {
         List<String> data = new ArrayList<>(parameters.get("prop"));
+        success = "Successfully unallocated the seat";
+        //Delete seat no and student data from classData(table)
         classDataService.deleteClassData(Long.parseLong(data.get(0)));
         return "redirect:/second/" + className;
     }
 
+    //Initialize the class and student objects, show empty values.
     @RequestMapping("/addNewData")
     public String addNewData(Model model) {
         model.addAttribute("classData", new ClassDetails());
@@ -115,6 +121,7 @@ public class StudentController {
         return "addClass";
     }
 
+    //Get the class details and save to database
     @PostMapping("/addClass")
     public String addClass(@ModelAttribute("classData") ClassDetails classDetails, Model model) {
         classDetails.setAvailability(100);
@@ -124,6 +131,7 @@ public class StudentController {
         return "addClass";
     }
 
+    //Get the student details and save to database
     @PostMapping("/deleteClass")
     public String deleteClass(@RequestParam MultiValueMap<String, String> parameters, Model model) {
         classDetailService.deleteClassDetail(new ArrayList<>(parameters.get("prop")).get(0));
@@ -132,6 +140,7 @@ public class StudentController {
         return "addClass";
     }
 
+    //Delete student from studentDetails(Table)
     @PostMapping("/deleteStudent")
     public String deleteStudent(@RequestParam MultiValueMap<String, String> parameters, Model model) {
         String rollNo = new ArrayList<>(parameters.get("prop2")).get(0);
@@ -142,6 +151,7 @@ public class StudentController {
         return "addClass";
     }
 
+    //Add new student details to database
     @PostMapping("/addStudent")
     public String addStudent(@ModelAttribute("student") StudentDetails studentDetails, Model model) {
         studentService.saveStudent(studentDetails);
@@ -150,6 +160,8 @@ public class StudentController {
         return "addClass";
     }
 
+
+    //Initialize the seat with value zero
     List<List<Integer>> emptyArray() {
         List<List<Integer>> a = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
@@ -167,14 +179,17 @@ public class StudentController {
         success = "";
         List<String> data = new ArrayList<>(parameters.get("prop"));
         for (int i = 0; i < data.size(); i++)
-            if (checkNumber(data.get(i))) {
-                if (checkValidation(className, Long.parseLong(data.get(i)), i + 1)) {
+            if (checkNumber(data.get(i)))
+                if(!checkSameClass(className, Long.parseLong(data.get(i))))
+                    error = "Already allocated seat in another class";
+                else if (checkValidation(className, Long.parseLong(data.get(i)), i + 1)) {
                     error = "Seat No: " + (i + 1) + " unable to arrange the seat";
                 } else {
                     success = "Successfully Allocated";
                     deleteClassData(className, i + 1);
+                    //Allocate the seat
                     saveClassData(className, Long.parseLong(data.get(i)), i + 1);
-                }
+
             }
         return "redirect:/second/" + className;
     }
@@ -186,29 +201,30 @@ public class StudentController {
                 classDataService.deleteClassData(classData.getRollNo());
     }
 
+    //Validate the seat with adjacent seats
     boolean checkValidation(String className, Long rollNo, int seatNo) {
         // If both are same discipline id return true
         List<ClassData> classDataList = classDataService.getAllClassData();
         StudentDetails studentDetails = studentService.getStudent(rollNo);
-        if (seatNo % 10 > 1) {
-//            System.out.println("prev");
-            for (int i = 0; i < classDataList.size(); i++) {
-                if (Objects.equals(classDataList.get(i).getClassName(), className) && Objects.equals(classDataList.get(i).getSeatNo(), seatNo - 1) && classDataList.get(i).getDisciplineId() == studentDetails.getDisciplineId())
+        if (seatNo % 10 > 1)
+            for (ClassData classData : classDataList)
+                if (Objects.equals(classData.getClassName(), className) && Objects.equals(classData.getSeatNo(), seatNo - 1) && classData.getDisciplineId() == studentDetails.getDisciplineId())
                     return true;
-//                System.out.println("prev false");
-            }
-        }
-        if (seatNo % 10 > 0) {
-//            System.out.println("next");
-            for (int i = 0; i < classDataList.size(); i++) {
-                if (Objects.equals(classDataList.get(i).getClassName(), className) && Objects.equals(classDataList.get(i).getSeatNo(), seatNo + 1) && classDataList.get(i).getDisciplineId() == studentDetails.getDisciplineId())
-                    return true;
-//                System.out.println("next true");
-            }
-        }
+
+        if (seatNo % 10 > 0)
+            for (ClassData classData : classDataList)
+                return Objects.equals(classData.getClassName(), className) && Objects.equals(classData.getSeatNo(), seatNo + 1) && classData.getDisciplineId() == studentDetails.getDisciplineId();
+
         return false;
     }
 
+    boolean checkSameClass(String className, Long rollNO){
+        System.out.println(className);
+        ClassData classData = classDataService.getClassData(rollNO);
+        return Objects.equals(classData.getClassName(), className);
+    }
+
+    //Save the student and seat no in ClassData(Table)
     void saveClassData(String className, Long rollNo, int seatNo) {
         StudentDetails studentDetails = studentService.getStudent(rollNo);
 //        System.out.println(rollNo);
--- src/test/java/com/example/sb/ProjectInfoApplicationTests.java
+++ src/test/java/com/example/sb/ProjectInfoApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.sb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 class ProjectInfoApplicationTests {
